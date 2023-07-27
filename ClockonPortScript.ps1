c76b3455846d452eea9b8acfed7ef9eeafca4345
KaiHaapy <140584457+KaiHaapy@users.noreply.github.com>
2023-07-27 13:57:47 +1000

    Update ClockonPortScript.ps1

--- ClockonPortScript.ps1
+++ ClockonPortScript.ps1
@@ -1,2 +1,2 @@
-﻿New-WebBinding -Name "clockon.livelifecentral.com" -Protocol https -Port 4444
-Get-ChildItem cert:\localmachine\ImportCert01 | New-Item -Path IIS:\SslBindings\!4444
+﻿New-WebBinding -Name "clockon.livelifecentral.com" -Protocol https -Port 4446
+Get-ChildItem cert:\localmachine\ImportCert01 | New-Item -Path IIS:\SslBindings\!4446
